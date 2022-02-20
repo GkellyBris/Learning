ed0c3ce8ae5a104ae1d6de4b041f65ce9efec6b3
GkellyBris <100052630+GkellyBris@users.noreply.github.com>
2022-02-20 00:25:26 +0000

    Add files via upload

---
+++ Userinput.py
@@ -0,0 +1,3 @@
+#user specifed value
+user = input('i am python, what is your name?':)
+print ('welcome', user)
---
+++ variable.py
@@ -0,0 +1,13 @@
+# initialise a variable with an intereg value
+from operator import truediv
+
+
+var = 8
+print(var)
+#ASSIGN A float value to variable
+var = 3.142
+print(var)
+var = 'python in easy steps'
+print(var)
+var='True'
+print(var)
